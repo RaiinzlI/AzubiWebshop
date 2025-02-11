4270159a9ea8515ac4caeba7358d8f36bc5b2e45
RaiinzlI <laurin.fauser@gmail.com>
2025-02-11 16:24:11 +0100

    GitIgnore and service templates

--- src/app/services/cart.services.ts
+++ src/app/services/cart.services.ts
@@ -2,12 +2,22 @@ import { DecimalPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
 import { NgZone } from '@angular/core';
-import { Product } from '../models/product.model';
-
+import { Produkt } from '../product-card/product-card.component';
+//import { Product } from '../product-card/product_interface';
 
 export class CartService {
 
-  constructor(private cdRef: ChangeDetectorRef, private ngZone: NgZone) { }
+  constructor(
+    private cdRef: ChangeDetectorRef, 
+    private ngZone: NgZone) { }
 
-  Products: Product[] = [];
-}
+  cartedProducts: Produkt[] = [];
+
+
+  addToCart(Product: Produkt): void {
+    console.log("Product added to cart");
+  }
+
+  removeFromCart(Product: Produkt): void {
+    console.log("Product removed from cart");
+  }
